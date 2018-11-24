f4d9354a8b5daa3725b0eaf2ff1130fb9d76f9bd
stanley bennett <stanley.bennett82@gmail.com>
2018-11-24 16:59:00 -0600

    Additional test cases

--- lib/test/payment_test.dart
+++ lib/test/payment_test.dart
@@ -2,6 +2,7 @@ import 'package:payment_tracker/model/payment.dart';
 import "package:test/test.dart";
 
 void main() {
+  // blue sky tests
   test("testBlueSky_defaultValues", () {
     // exercise
     Payment payment = new Payment("Capital One");
@@ -11,5 +12,46 @@ void main() {
     expect(DateTime.now().day, payment.day);
     expect(null, payment.id);
     expect("Capital One", payment.name);
+  });
+
+  test("testBlueSky_setAmount", () {
+    // set-up
+    Payment payment = new Payment("Capital One");
+
+    // pre-conditions
+    expect(0.0, payment.amount);
+
+    // exercise
+    payment.setAmount("85.93");
+
+    // post-conditions
+    expect(85.93, payment.amount);
+  });
+
+  test("testBlueSky_setDate", () {
+    // set-up
+    Payment payment = new Payment("Capital One");
+    DateTime date = new DateTime(2018, 11, 24);
+
+    // exercise
+    payment.setDate(date);
+
+    // post-conditions
+    expect(date, DateTime.fromMillisecondsSinceEpoch(payment.date));
+  });
+
+  // non blue sky tests
+  test("testNonBlueSky_setAmount_withInvalidValue", () {
+    // set-up
+    Payment payment = new Payment("Capital One");
+
+    // pre-conditions
+    expect(0.0, payment.amount);
+
+    // exercise
+    payment.setAmount("abc");
+
+    // post-conditions
+    expect(0.0, payment.amount);
   });
 }
